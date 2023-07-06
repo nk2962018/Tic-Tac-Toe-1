a628e0d8ffb45443949eca30419699e195a07118
Nisha <nkolivia1234@gmail.com>
2023-07-06 16:19:09 +0530

    Feat:declare a winner when a player marks all tiles in the second row.

--- src/components/constants/TestConstants.js
+++ src/components/constants/TestConstants.js
@@ -6,17 +6,19 @@ export const TestConstants = {
     PLAYER_O: "O",
     NEXT_PLAYER_TURN_MESSAGE: "Next Player : ",
     DECLARE_WINNER_MESSAGE: 'The Winner is : ',
-    FIRST_ROW_WINNING_POSITION: [0,1,2]
+    FIRST_ROW_WINNING_POSITION: [0,1,2],
+    SECOND_ROW_WINNING_POSITION: [3,4,5]
     
 };
 
 export const WINNING_COMBINATIONS = [
     TestConstants.FIRST_ROW_WINNING_POSITION,
+    TestConstants.SECOND_ROW_WINNING_POSITION
 
 ];
 
 export const tilePositionsToDeclareWinner = {
-    
+
     winningMovesFirstRow_X : {
         WINING_POSITIONS : [0,3,1,6,2],
         WINNER : TestConstants.PLAYER_X
@@ -25,6 +27,16 @@ export const tilePositionsToDeclareWinner = {
     winningMovesFirstRow_O : {
         WINING_POSITIONS : [3,0,6,1,4,2],
         WINNER : TestConstants.PLAYER_O
-    }
+    },
+
+    winningMovesSecondRow_X : {
+        WINING_POSITIONS : [3,6,4,1,5],
+        WINNER : TestConstants.PLAYER_X
+    },
+
+    winningMovesSecondRow_O : {
+        WINING_POSITIONS : [1,3,6,4,2,5],
+        WINNER : TestConstants.PLAYER_O
+    },
 
 }
--- src/testcases/TicTacToe.spec.js
+++ src/testcases/TicTacToe.spec.js
@@ -87,5 +87,13 @@ describe("TicTacToe game works fine when" , () => {
     declareWinnerOnMarkingDesiredPositions(tilePositionsToDeclareWinner.winningMovesFirstRow_O)
   });
 
+  it(("displays winning message for player X when it marks all the second row winning positions"), () => {
+    declareWinnerOnMarkingDesiredPositions(tilePositionsToDeclareWinner.winningMovesSecondRow_X)
+  });
+
+  it(("displays winning message for player O when it marks all the second row winning positions"), () => {
+    declareWinnerOnMarkingDesiredPositions(tilePositionsToDeclareWinner.winningMovesSecondRow_O)
+  });
+
 });
 
